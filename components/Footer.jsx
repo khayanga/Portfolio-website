7e1fcd041e3ebdfafafa7e585da93b4606f8a535
Virgil Khayanga <117723011+khayanga@users.noreply.github.com>
2026-02-23 10:17:51 +0300

    Year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,5 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-
   return (
     <footer className="py-8 px-6 border-t border-border">
       <div className="container mx-auto max-w-6xl">
